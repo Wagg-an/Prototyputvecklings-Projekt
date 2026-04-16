c6ba44548677046fa8800d1c8bb018288fe9fbc8
Wagg-an <oliwah4637@gmail.com>
2026-04-16 22:11:06 +0200

    PuzzleCode

--- Assets/Olivers_Skit/Scripts/PuzzleBox.cs
+++ Assets/Olivers_Skit/Scripts/PuzzleBox.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class PuzzleBox : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int keyCount = 0;
+    public Animator animator;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("key"))
+        {
+            Destroy(other.gameObject);
+            keyCount++;
+
+            Debug.Log("Key collected: " + keyCount);
+
+            TriggerAnimation();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void TriggerAnimation()
     {
-        
+        if (keyCount == 1)
+        {
+            animator.SetTrigger("Key1");
+        }
+        else if (keyCount == 2)
+        {
+            animator.SetTrigger("Key2");
+        }
+        else if (keyCount == 3)
+        {
+            animator.SetTrigger("Key3");
+        }
     }
 }
