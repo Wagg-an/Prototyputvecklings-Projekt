6b0ab75bf1dff5c61d312abf23c0829206c4b268
Wagg-an <oliwah4637@gmail.com>
2026-05-06 17:23:46 +0200

    Stuff

--- Assets/Olivers_Skit/Scripts/PuzzleBox.cs
+++ Assets/Olivers_Skit/Scripts/PuzzleBox.cs
@@ -4,14 +4,17 @@ public class PuzzleBox : MonoBehaviour
 {
     public int keyCount = 0;
     public Animator animator;
+    private string form = "";
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("key"))
         {
+            form = other.name;
             Destroy(other.gameObject);
             keyCount++;
 
+            Debug.Log(form);
             Debug.Log("Key collected: " + keyCount);
 
             TriggerAnimation();
@@ -20,17 +23,22 @@ public class PuzzleBox : MonoBehaviour
 
     void TriggerAnimation()
     {
-        if (keyCount == 1)
+        if (form == "redCube")
         {
             animator.SetTrigger("Key1");
         }
-        else if (keyCount == 2)
+        else if (form == "blueCylinder")
         {
             animator.SetTrigger("Key2");
         }
-        else if (keyCount == 3)
+        else if (form == "yellowTriangle")
         {
             animator.SetTrigger("Key3");
+        }
+
+        if(keyCount == 3)
+        {
+            animator.SetTrigger("Open");
         }
     }
 }
